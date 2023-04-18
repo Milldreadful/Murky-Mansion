7824621bdc5b125891092a320ff85c9197a7edba
Milldreadful <128032585+Milldreadful@users.noreply.github.com>
2023-04-18 15:01:52 +0300

    Walk+jump animation, WIP water spray

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -10,6 +10,8 @@ public class PlayerControllerScript : MonoBehaviour
     public Rigidbody rb;
     bool grounded;
 
+    public Animator walk;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +25,30 @@ public class PlayerControllerScript : MonoBehaviour
         float depth = Input.GetAxis("Depth");
 
         Vector3 movement = new Vector3(horizontal, 0.0f, depth);
-        transform.rotation = Quaternion.LookRotation(movement);
 
 
         transform.Translate(movement * playerSpeed * Time.deltaTime, Space.World);
 
-        //transform.Translate(Vector3.right * horizontal * playerSpeed * Time.deltaTime);
-        //transform.Translate(Vector3.forward * depth * playerSpeed * Time.deltaTime);
-
-
         if (Input.GetButtonDown("Jump") && grounded)
         {
+            walk.SetTrigger("Jump");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
+
+        //transform.Translate(Vector3.right * horizontal * playerSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * depth * playerSpeed * Time.deltaTime);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+            walk.SetBool("IsMoving", true);
+        }
+
+        else
+        {
+            walk.SetBool("IsMoving", false);
+        }
     }
 
     public void OnCollisionEnter(Collision collision)
